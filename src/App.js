34c2ea582756d641e170985397298b3f936c0cfa
GrigoryevaEva <grigoryeva.eva.2704@mail.ru>
2023-10-27 10:43:33 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Costs from "./components/Costs/Costs";
+import AddCostItem from "./components/AddCostItem/AddCostItem";
+
+import "./index.scss";
+
+const INITIAL_COSTS = [
+  {
+    id: "c1",
+    date: new Date(2023, 2, 12),
+    title: "Movie",
+    price: 10,
+  },
+  {
+    id: "c2",
+    date: new Date(2022, 9, 25),
+    title: "Macbook",
+    price: 1230,
+  },
+  {
+    id: "c3",
+    date: new Date(2021, 3, 11),
+    title: "Rent",
+    price: 600,
+  },
+  {
+    id: "c4",
+    date: new Date(2023, 4, 12),
+    title: "Gym membership",
+    price: 99.99,
+  },
+  {
+    id: "c5",
+    date: new Date(2022, 6, 25),
+    title: "Case for documents",
+    price: 560,
+  },
+  {
+    id: "c6",
+    date: new Date(2021, 11, 11),
+    title: "Restaurant",
+    price: 336.58,
+  },
+];
 
 function App() {
+  const [costs, setCosts] = useState(INITIAL_COSTS);
+
+  function handleAddNewCostItem(cost) {
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-main">
+      <AddCostItem onAddNewCostItem={handleAddNewCostItem} />
+      <Costs costs={costs} />
     </div>
   );
 }
